5c8035f71e9d6a65a3410a3925a87e572f3232d1
Ram-Ram007 <ramajayam2411@gmail.com>
2023-11-01 23:55:24 +0530

    modified

--- app/src/components/Todolist.tsx
+++ app/src/components/Todolist.tsx
@@ -4,25 +4,31 @@ interface IAddTodo {
   onTodoAdd: (str: string) => void;
 }
 
-
-
-const AddTodo: React.FC<IAddTodo> = ({ onTodoAdd }) => {
+const Display: React.FC<IAddTodo> = ({ onTodoAdd }) => {
   const [text, setText] = useState("");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    onTodoAdd(text);
 
-  function handleSubmit(){
-    onTodoAdd()
+    setText("");
   }
+
   return (
     <>
       <h1>List app</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="input">Enter List</label>
-        <input type="test" id="input" />
+        <input
+          type="test"
+          id="input"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <button type="submit">Click</button>
       </form>
     </>
   );
-}
+};
 
 export default Display;
