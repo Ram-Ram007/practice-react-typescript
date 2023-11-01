9baf3f36a7f16f5d68a83fb6e70260a33f8b20cf
Ram-Ram007 <ramajayam2411@gmail.com>
2023-11-02 00:01:25 +0530

    modified

--- app/src/components/List.tsx
+++ app/src/components/List.tsx
@@ -1,11 +1,17 @@
 import { ITodo } from "../type";
 
+interface ITodoList {
+  todos: ITodo[];
+  extraCss?: string;
+}
 
-
-const TodoList: React.FC<ITodoList> = ({ todos, extraCss }) => {  return (
-    <ul>
-      <li>List one</li>
+const List: React.FC<ITodoList> = ({ todos, extraCss }) => {
+  return (
+    <ul className={extraCss}>
+      {todos.map((t) => (
+        <li key={t.id.toString()}>{t.text}</li>
+      ))}
     </ul>
   );
-}
+};
 export default List;
