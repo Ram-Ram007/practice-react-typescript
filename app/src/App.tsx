4586fefa9ae6450f4dc283424e376982928e2b39
Ram-Ram007 <ramajayam2411@gmail.com>
2023-11-01 21:03:01 +0530

    first commit

---
+++ app/src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+import "./App.css";
+
+import Display from "./components/Todolist";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return <Display />;
+}
+export default App;
---
+++ app/src/components/Todolist.tsx
@@ -0,0 +1,7 @@
+
+
+function Display() {
+  return <h1>hello</h1>;
+}
+
+export default Display;
