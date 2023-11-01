64b087f4dd52c421c79e65cd0a74e1b06fcbab98
Ram-Ram007 <ramajayam2411@gmail.com>
2023-11-01 21:30:37 +0530

    added type file

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,21 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 import Display from "./components/Todolist";
 import List from "./components/List";
+import { ITodo } from "./type";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+
+  function onTodoAdd(str: string) {
+    const obj: ITodo = {
+      text: str,
+      id: new Date().getTime(),
+    };
+    setTodos((prev) => [...prev, obj]);
+  }
 
   return (
     <>
